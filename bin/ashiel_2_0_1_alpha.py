f5f63ed87f7390fa9b5cbb69ccbccc33fb4521f4
Connor O <connor607@gmail.com>
2018-05-20 11:22:34 -0500

    TkInter Testing

--- bin/ashiel_2_0_1_alpha.py
+++ bin/ashiel_2_0_1_alpha.py
@@ -9,65 +9,65 @@ except ImportError:
     import tkinter.scrolledtext as ScrolledText
 import time
 
-"""
-This example shows how to create a chat bot that
-will learn responses based on an additional feedback
-element from the user.
-"""
 
-# Uncomment the following line to enable verbose logging
-# import logging
-# logging.basicConfig(level=logging.INFO)
+class TkinterGUIExample(tk.Tk):
 
-# Create a new instance of a ChatBot
-bot = ChatBot(
-    'ASHIEL 2',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.BestMatch'
-    ],
-    input_adapter='chatterbot.input.TerminalAdapter',
-    output_adapter='chatterbot.output.TerminalAdapter'
-)
+    def __init__(self, *args, **kwargs):
+        """
+        Create & set window variables.
+        """
+        tk.Tk.__init__(self, *args, **kwargs)
 
-bot.set_trainer(ChatterBotCorpusTrainer)
+        self.chatbot = ChatBot(
+            "GUI Bot",
+            storage_adapter="chatterbot.storage.SQLStorageAdapter",
+            logic_adapters=[
+                "chatterbot.logic.BestMatch"
+            ],
+            input_adapter="chatterbot.input.VariableInputTypeAdapter",
+            output_adapter="chatterbot.output.OutputAdapter",
+            database="../database.db"
+        )
 
-bot.train("chatterbot.corpus.english")
+        self.title("Chatterbot")
 
-CONVERSATION_ID = bot.storage.create_conversation()
+        self.initialize()
+
+    def initialize(self):
+        """
+        Set window layout.
+        """
+        self.grid()
+
+        self.respond = ttk.Button(self, text='Get Response', command=self.get_response)
+        self.respond.grid(column=0, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.usr_input = ttk.Entry(self, state='normal')
+        self.usr_input.grid(column=1, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.conversation_lbl = ttk.Label(self, anchor=tk.E, text='Conversation:')
+        self.conversation_lbl.grid(column=0, row=1, sticky='nesw', padx=3, pady=3)
+
+        self.conversation = ScrolledText.ScrolledText(self, state='disabled')
+        self.conversation.grid(column=0, row=2, columnspan=2, sticky='nesw', padx=3, pady=3)
+
+    def get_response(self):
+        """
+        Get a response from the chatbot and display it.
+        """
+        user_input = self.usr_input.get()
+        self.usr_input.delete(0, tk.END)
+
+        response = self.chatbot.get_response(user_input)
+
+        self.conversation['state'] = 'normal'
+        self.conversation.insert(
+            tk.END, "Human: " + user_input + "\n" + "ChatBot: " + str(response.text) + "\n"
+        )
+        self.conversation['state'] = 'disabled'
+
+        time.sleep(0.5)
 
 
-def get_feedback():
-    from chatterbot.utils import input_function
-
-    text = input_function()
-
-    if 'yes' in text.lower():
-        return True
-    elif 'no' in text.lower():
-        return False
-    else:
-        print('Please type either "Yes" or "No"')
-        return get_feedback()
-
-
-print('Type something to begin...')
-
-# The following loop will execute each time the user enters input
-while True:
-    try:
-        input_statement = bot.input.process_input_statement()
-        statement, response = bot.generate_response(input_statement, CONVERSATION_ID)
-        print('\n Is "{}" this a coherent response to "{}"? \n'.format(response, input_statement))
-
-        if get_feedback():
-            bot.learn_response(response, input_statement)
-            # Update the conversation history for the bot
-            # It is important that this happens last, after the learning step
-            bot.storage.add_to_conversation(CONVERSATION_ID, statement, response)
-
-        bot.output.process_response(response)
-
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        break
+gui_example = TkinterGUIExample()
+gui_example.mainloop()
