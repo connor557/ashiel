a471abab1425b8194e37ac021adfa5748dcc30c4
Connor O <connor607@gmail.com>
2018-05-20 15:10:31 -0500

    Fix logic+corpus model baased on example

--- bin/ashiel_2_0_1_alpha.py
+++ bin/ashiel_2_0_1_alpha.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 import tkinter as tk
 try:
     import ttk as ttk
@@ -9,30 +9,21 @@ except ImportError:
     import tkinter.scrolledtext as ScrolledText
 import time
 
-"""
-This example shows how to create a chat bot that
-will learn responses based on an additional feedback
-element from the user.
-"""
-
 # Uncomment the following line to enable verbose logging
 # import logging
 # logging.basicConfig(level=logging.INFO)
 
 # Create a new instance of a ChatBot
 bot = ChatBot(
-    'ASHIEL 2',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.BestMatch'
-    ],
-    input_adapter='chatterbot.input.TerminalAdapter',
-    output_adapter='chatterbot.output.TerminalAdapter'
+    "ASHIEL 2",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    input_adapter="chatterbot.input.TerminalAdapter",
+    output_adapter="chatterbot.output.TerminalAdapter"
 )
-
 bot.set_trainer(ChatterBotCorpusTrainer)
 
 bot.train("chatterbot.corpus.english")
+
 
 CONVERSATION_ID = bot.storage.create_conversation()
 
@@ -43,30 +34,30 @@ def get_feedback():
     text = input_function()
 
     if 'yes' in text.lower():
-        return True
-    elif 'no' in text.lower():
         return False
+    elif 'no' in text.lower():
+        return True
     else:
         print('Please type either "Yes" or "No"')
         return get_feedback()
 
 
-print('Type something to begin...')
+print("Type something to begin...")
 
 # The following loop will execute each time the user enters input
 while True:
     try:
         input_statement = bot.input.process_input_statement()
         statement, response = bot.generate_response(input_statement, CONVERSATION_ID)
-        print('\n Is "{}" this a coherent response to "{}"? \n'.format(response, input_statement))
-
-        if get_feedback():
-            bot.learn_response(response, input_statement)
-            # Update the conversation history for the bot
-            # It is important that this happens last, after the learning step
-            bot.storage.add_to_conversation(CONVERSATION_ID, statement, response)
 
         bot.output.process_response(response)
+        print('\n Is "{}" a coherent response to "{}"? \n'.format(response, input_statement))
+        if get_feedback():
+            print("please input the correct one")
+            response1 = bot.input.process_input_statement()
+            bot.learn_response(response1, input_statement)
+            bot.storage.add_to_conversation(CONVERSATION_ID, statement, response1)
+            print("Responses added to bot!")
 
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
